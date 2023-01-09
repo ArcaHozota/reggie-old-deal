a07fc311fc9152b462ba52dbb87f045ef9ef6353
ArcaHozota <arkamahozota@gmail.com>
2023-01-09 21:57:26 +0900

    1.17

--- src/main/java/com/itheima/reggie/config/SnowflakeProperties.java
+++ src/main/java/com/itheima/reggie/config/SnowflakeProperties.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
 
+/**
+ * @author Administrator
+ */
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "snowflake")
