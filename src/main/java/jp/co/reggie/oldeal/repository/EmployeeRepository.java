50b9113dc965374e2384b2d943bc0a921881650f
ArcaHozota <arkamahozota@gmail.com>
2023-02-10 12:15:11 +0900

    1.43

---
+++ src/main/java/jp/co/reggie/oldeal/repository/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package jp.co.reggie.oldeal.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import jp.co.reggie.oldeal.entity.Employee;
+
+/**
+ * 員工管理實體類接口
+ *
+ * @author Administrator
+ */
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+	Page<Employee> getByNames(@Param("name") String keyword, Pageable pageable);
+}
