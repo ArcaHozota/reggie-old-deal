f61ab6d844f2c3f22dcb98b439bd923bf9a68dc5
ArcaHozota <arkamahozota@gmail.com>
2023-03-14 21:42:20 +0900

    2.05

--- src/main/java/jp/co/reggie/oldeal/controller/OrdersController.java
+++ src/main/java/jp/co/reggie/oldeal/controller/OrdersController.java
@@ -1,10 +1,17 @@
 package jp.co.reggie.oldeal.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import jp.co.reggie.oldeal.entity.Orders;
 import jp.co.reggie.oldeal.service.IOrdersService;
+import jp.co.reggie.oldeal.utils.Reggie;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -19,4 +26,14 @@ public class OrdersController {
 
 	@Autowired
 	private IOrdersService ordersService;
+
+	public Reggie<Page<Orders>> pagination(@RequestParam("pageNum") final Integer pageNum,
+			@RequestParam("pageSize") final Integer pageSize,
+			@RequestParam(value = "number", required = false) final Long orderId,
+			@RequestParam(value = "beginTime", required = false) final LocalDateTime beginTime,
+			@RequestParam(value = "terminalTime", required = false) final LocalDateTime terminalTime) {
+		final Page<Orders> pageInfo = this.ordersService.pagination(pageNum, pageSize, orderId, beginTime,
+				terminalTime);
+		return Reggie.success(pageInfo);
+	}
 }
