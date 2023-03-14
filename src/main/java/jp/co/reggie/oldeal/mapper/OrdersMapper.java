c42e29a5603d5d4e9b237ae102c3465604d1ddf6
ArcaHozota <arkamahozota@gmail.com>
2023-03-14 21:11:20 +0900

    1.99

---
+++ src/main/java/jp/co/reggie/oldeal/mapper/OrdersMapper.java
@@ -0,0 +1,14 @@
+package jp.co.reggie.oldeal.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import jp.co.reggie.oldeal.entity.Orders;
+
+/**
+ * 訂單實體類接口
+ *
+ * @author Administrator
+ * @date 2023-02-18
+ */
+public interface OrdersMapper extends BaseMapper<Orders> {
+}
