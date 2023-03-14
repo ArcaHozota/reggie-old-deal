dc9d49208242be0d51065623b134ae4625b8effe
ArcaHozota <arkamahozota@gmail.com>
2023-03-14 21:14:57 +0900

    2.00

---
+++ src/main/java/jp/co/reggie/oldeal/service/OrdersService.java
@@ -0,0 +1,13 @@
+package jp.co.reggie.oldeal.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import jp.co.reggie.oldeal.entity.Orders;
+
+/**
+ *
+ * @author Administrator
+ *
+ */
+public interface OrdersService extends IService<Orders> {
+}
---
+++ src/main/java/jp/co/reggie/oldeal/service/impl/OrdersServiceImpl.java
@@ -0,0 +1,17 @@
+package jp.co.reggie.oldeal.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import jp.co.reggie.oldeal.entity.Orders;
+import jp.co.reggie.oldeal.mapper.OrdersMapper;
+import jp.co.reggie.oldeal.service.OrdersService;
+
+/**
+ * @author Administrator
+ * @date 2023-02-18
+ */
+@Service
+public class OrdersServiceImpl extends ServiceImpl<OrdersMapper, Orders> implements OrdersService {
+}
