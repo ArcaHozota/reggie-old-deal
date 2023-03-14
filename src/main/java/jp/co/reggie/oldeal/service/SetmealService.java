2ee13439469cce17bf0d65b06d005352378a8499
ArcaHozota <arkamahozota@gmail.com>
2023-03-14 14:59:36 +0900

    1.83

--- src/main/java/jp/co/reggie/oldeal/service/SetmealService.java
+++ src/main/java/jp/co/reggie/oldeal/service/SetmealService.java
@@ -2,6 +2,7 @@ package jp.co.reggie.oldeal.service;
 
 import java.util.List;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 import jp.co.reggie.oldeal.dto.SetmealDto;
@@ -25,4 +26,14 @@ public interface SetmealService extends IService<Setmeal> {
 	 * @param ids 套餐ID的集合
 	 */
 	void removeWithDish(List<Long> ids);
+
+	/**
+	 * 進行分頁
+	 *
+	 * @param pageNum  頁碼
+	 * @param pageSize 頁面大小
+	 * @param keyword  檢索文
+	 * @return Page<SetmealDto>
+	 */
+	Page<SetmealDto> pagination(Integer pageNum, Integer pageSize, String keyword);
 }
--- src/main/java/jp/co/reggie/oldeal/service/impl/SetmealServiceImpl.java
+++ src/main/java/jp/co/reggie/oldeal/service/impl/SetmealServiceImpl.java
@@ -3,20 +3,24 @@ package jp.co.reggie.oldeal.service.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.annotation.Resource;
-
 import org.postgresql.util.PSQLException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 import jp.co.reggie.oldeal.common.CustomException;
 import jp.co.reggie.oldeal.common.CustomMessage;
 import jp.co.reggie.oldeal.dto.SetmealDto;
+import jp.co.reggie.oldeal.entity.Category;
 import jp.co.reggie.oldeal.entity.Setmeal;
 import jp.co.reggie.oldeal.entity.SetmealDish;
+import jp.co.reggie.oldeal.mapper.CategoryMapper;
 import jp.co.reggie.oldeal.mapper.SetmealMapper;
 import jp.co.reggie.oldeal.service.SetmealDishService;
 import jp.co.reggie.oldeal.service.SetmealService;
@@ -29,9 +33,15 @@ import jp.co.reggie.oldeal.service.SetmealService;
 public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService {
 
 	/**
+	 * 分類管理實體類接口
+	 */
+	@Autowired
+	private CategoryMapper categoryMapper;
+
+	/**
 	 * 套餐與菜品服務類
 	 */
-	@Resource
+	@Autowired
 	private SetmealDishService setmealDishService;
 
 	/**
@@ -77,5 +87,52 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
 		final LambdaQueryWrapper<SetmealDish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
 		lambdaQueryWrapper.in(SetmealDish::getSetmealId, ids);
 		this.setmealDishService.remove(lambdaQueryWrapper);
+	}
+
+	/**
+	 * 進行分頁
+	 *
+	 * @param pageNum  頁碼
+	 * @param pageSize 頁面大小
+	 * @param keyword  檢索文
+	 * @return Page<SetmealDto>
+	 */
+	@Override
+	public Page<SetmealDto> pagination(final Integer pageNum, final Integer pageSize, final String keyword) {
+		// 聲明分頁構造器；
+		Page<Setmeal> pageInfo = Page.of(pageNum, pageSize);
+		// 聲明條件構造器；
+		final LambdaQueryWrapper<Setmeal> queryWrapper = Wrappers.lambdaQuery(new Setmeal());
+		// 添加查詢條件，根據檢索文進行模糊查詢；
+		queryWrapper.like(!keyword.isBlank(), Setmeal::getName, keyword);
+		// 添加排序條件；
+		queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+		// 執行查詢；
+		pageInfo = this.page(pageInfo, queryWrapper);
+		// 聲明數據傳輸類分頁構造器；
+		final Page<SetmealDto> dtoPage = new Page<>();
+		// 拷貝屬性；
+		BeanUtils.copyProperties(pageInfo, dtoPage, "records");
+		// 獲取數據傳輸類分頁；
+		final List<SetmealDto> records = pageInfo.getRecords().stream().map((item) -> {
+			// 聲明套餐數據傳輸類；
+			final SetmealDto setmealDto = new SetmealDto();
+			// 對象拷貝；
+			BeanUtils.copyProperties(item, setmealDto);
+			// 獲取分類ID；
+			final Long categoryId = item.getCategoryId();
+			// 根據分類ID獲取分類對象；
+			final Category category = this.categoryMapper.selectById(categoryId);
+			// 分類對象存在；
+			if (category != null) {
+				// 獲取分類名稱並設置到數據傳輸類中；
+				final String categoryName = category.getName();
+				setmealDto.setCategoryName(categoryName);
+			}
+			return setmealDto;
+		}).collect(Collectors.toList());
+		// 設置分頁數據於構造器中並返回；
+		dtoPage.setRecords(records);
+		return dtoPage;
 	}
 }
