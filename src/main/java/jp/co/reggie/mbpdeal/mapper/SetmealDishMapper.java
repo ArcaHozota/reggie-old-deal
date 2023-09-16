42874e3b1d043f8f0ff356e30045df092a38bcdc
Arca Hozota <arkamahozota@gmail.com>
2023-09-16 11:31:47 +0900

    3.26

--- src/main/java/jp/co/reggie/mbpdeal/mapper/SetmealDishMapper.java
+++ src/main/java/jp/co/reggie/mbpdeal/mapper/SetmealDishMapper.java
@@ -1,6 +1,9 @@
 package jp.co.reggie.mbpdeal.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
@@ -14,4 +17,11 @@ import jp.co.reggie.mbpdeal.entity.SetmealDish;
  */
 @Mapper
 public interface SetmealDishMapper extends BaseMapper<SetmealDish> {
+
+	/**
+	 * 根據套餐ID批量執行刪除
+	 *
+	 * @param smIdList 套餐ID集合
+	 */
+	void batchRemoveBySmIds(@Param("smIdList") List<Long> smIdList);
 }
