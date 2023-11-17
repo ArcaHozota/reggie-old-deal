48649ba45bf270c24c75c3a1480bcfd043e5248d
ArkamaHozota <arkamahozota@gmail.com>
2023-11-17 10:54:06 +0900

    3.52

--- src/main/java/jp/co/reggie/mbpdeal/handler/LoginCheckFilter.java
+++ src/main/java/jp/co/reggie/mbpdeal/handler/LoginCheckFilter.java
@@ -16,15 +16,14 @@ import org.springframework.util.AntPathMatcher;
 import com.alibaba.fastjson.JSON;
 
 import jp.co.reggie.mbpdeal.common.Constants;
-import jp.co.reggie.mbpdeal.entity.Employee;
-import jp.co.reggie.mbpdeal.entity.User;
 import jp.co.reggie.mbpdeal.utils.Reggie;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * 登錄検査過濾器
  *
- * @author Administrator
+ * @author ArcaHozota
+ * @since 1.00beta
  */
 @Slf4j
 @WebFilter(filterName = "loginCheckFilter", urlPatterns = "/*")
@@ -35,6 +34,23 @@ public class LoginCheckFilter implements Filter {
 	 */
 	private static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
 
+	/**
+	 * 検査本次請求是否位於無需過濾的路徑集合
+	 *
+	 * @param requestURI 本次請求路徑
+	 * @param urls       無需過濾的路徑集合
+	 * @return true：滿足，false：不滿足
+	 */
+	private boolean check(final String requestURI, final String... urls) {
+		for (final String url : urls) {
+			final boolean match = PATH_MATCHER.match(url, requestURI);
+			if (match) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	@Override
 	public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse,
 			final FilterChain filterChain) throws IOException, ServletException {
@@ -43,11 +59,11 @@ public class LoginCheckFilter implements Filter {
 		// 獲取請求的URI路徑；
 		final String requestURI = request.getRequestURI();
 		// 定義無需過濾的路徑集合；
-		final String[] urls = new String[] { "/employee/login", "/employee/logout", "/front/**", "/backend/**",
-				"/common/**", "/user/login", "/user/sendMsg" };
+		final String[] urls = { "/employee/login", "/employee/logout", "/front/**", "/backend/**", "/common/**",
+				"/user/login", "/user/sendMsg" };
 		// 獲取用戸ID；
-		final Long empId = (Long) request.getSession().getAttribute(Constants.getEntityName(new Employee()));
-		final Long userId = (Long) request.getSession().getAttribute(Constants.getEntityName(new User()));
+		final Long empId = (Long) request.getSession().getAttribute("employee");
+		final Long userId = (Long) request.getSession().getAttribute("user");
 		// 判斷本次請求是否需要處理，如果勿需處理，則直接放行；
 		if (this.check(requestURI, urls)) {
 			log.info("本次請求{}不需要處理", requestURI);
@@ -69,22 +85,5 @@ public class LoginCheckFilter implements Filter {
 		// 如果未登錄，則返回未登錄結果；
 		log.info("訪問失敗：{}", Constants.NOT_LOGIN);
 		response.getWriter().write(JSON.toJSONString(Reggie.error(Constants.NOT_LOGIN)));
-	}
-
-	/**
-	 * 検査本次請求是否位於無需過濾的路徑集合
-	 *
-	 * @param requestURI 本次請求路徑
-	 * @param urls       無需過濾的路徑集合
-	 * @return true：滿足，false：不滿足
-	 */
-	private boolean check(final String requestURI, final String... urls) {
-		for (final String url : urls) {
-			final boolean match = PATH_MATCHER.match(url, requestURI);
-			if (match) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
